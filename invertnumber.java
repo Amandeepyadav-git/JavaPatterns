3507bfa9b6c3948bd81e9176e4a129ea3b646be1
Amandeep Yadav <amandeepyadav0704@gmail.com>
2025-12-31 14:00:27 +0530

    inverted files

---
+++ invertnumber.java
@@ -0,0 +1,13 @@
+// inverted right angle triangle 5 to 1
+
+public class invertnumber {
+    public static void main(String[] args) {
+        int n =5;
+        for(int i = n;i>=1;i--){
+            for(int j = 1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ strnumber.java
@@ -0,0 +1,14 @@
+//right angle pattern 1 to 5 in java
+
+public class strnumber {
+    public static void main(String[] args) {
+        int n =5 ;
+        for(int i = 1; i<=n;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+}
