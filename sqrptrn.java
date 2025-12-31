d727b4a9bf7ce0621714da1f1048e29d026282d6
Amandeep Yadav <amandeepyadav0704@gmail.com>
2025-12-31 13:41:18 +0530

    first commit

---
+++ sqrptrn.java
@@ -0,0 +1,13 @@
+//To print star square pattern 5x5
+
+public class sqrptrn {
+    public static void main(String[] args) {
+        int n =5;
+        for(int i=1;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
