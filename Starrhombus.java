c81af059c874949d3eb599b3a4f4c280461dc360
Amandeep Yadav <amandeepyadav0704@gmail.com>
2026-01-04 10:58:31 +0530

    new commits

---
+++ Starrhombus.java
@@ -0,0 +1,25 @@
+// ____****
+// ___****_
+// __****__
+// _****___
+// ****___
+
+
+public class Starrhombus {
+    public static void main(String[] args) {
+        int n =4;
+        int s =5;
+        for(int i  = 1; i<=s;i++){
+            for(int j = 1; j<=s-i;j++){
+                System.out.print("_");
+            }
+            for(int j =0;j<s;j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=i-1;j++){
+                System.out.print("_");
+            }
+            System.out.println();
+        }
+    }
+}
